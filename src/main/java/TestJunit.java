1193397b9cdf5e3defe6fe8cd434a9df77aa444c
陈袁 <chengcheng222e@sina.com>
2014-04-20 20:55:35 +0800

    test

---
+++ src/main/java/TestJunit.java
@@ -0,0 +1,9 @@
+/**
+ * Created by chenyuan on 4/20/14.
+ */
+public class TestJunit {
+
+    public static void main(String[] args) {
+        System.out.println("==============");
+    }
+}
---
+++ src/main/java/TestStruts2.java
@@ -0,0 +1,5 @@
+/**
+ * Created by chenyuan on 4/20/14.
+ */
+public class TestStruts2 {
+}
